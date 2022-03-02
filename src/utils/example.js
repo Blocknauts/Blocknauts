822fc84b66d8c2e751de64be4b7a3f60528bed83
Sam Rogers <sam.0110.rogers@gmail.com>
2022-03-02 10:06:14 -0600

    Configure Webpack and Babel, return hardcoded JSON for now
    I made sure the build is working

---
+++ src/utils/example.js
@@ -0,0 +1,12 @@
+const preferences = {
+  colors: {
+    mode: 'dark',
+    highlight: 'purple',
+    font: '#131313,',
+  },
+  cookies: {
+    accept: 'strictly-necessary',
+  },
+};
+
+export { preferences }
